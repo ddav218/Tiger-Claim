a1dc7245506698570d476131fad3c725f6e4d8dc
Darrius Davidson <ddav218@lsu.edu>
2025-11-06 21:20:54 -0600

    added tiger claim logo and updated navBar

--- tigerclaim/src/FrontDev/components/navBar.js
+++ tigerclaim/src/FrontDev/components/navBar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../../styles/navBar.css";
+import TigerLogo from "../assets/TigerClaimLogoWhite.png";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Tiger Claim</div>
+      <div className="navbar-logo-container">
+        <img src={TigerLogo} alt="Tiger Claim Logo" className="navbar-logo-img" />
+      </div>
       <div className="navbar-links">
         <button className="nav-btn">Lost Items</button>
         <button className="nav-btn">Found Items</button>
